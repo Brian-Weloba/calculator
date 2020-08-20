17f4d5be7863b6f19bc3ea20d8cf4880a70e79ee
Brian-Weloba <bweloba@gmail.com>
2020-08-20 09:07:10 +0300

    add subtraction function

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ var add = function(number1, number2) {
     return number1 + number2;
 };
 
+var subtract = function(number1, number2) {
+    return number1 - number2;
+};
+
 var number1 = parseInt(prompt("Input number:"));
 var number2 = parseInt(prompt("Input another number:"));
 
