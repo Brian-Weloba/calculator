e1a6cf50fd23ae61919749e759496e4db0abf43f
Brian-Weloba <bweloba@gmail.com>
2020-08-20 09:09:24 +0300

    add multiply function to .js

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ var divide = function(number1, number2) {
     return number1 / number2;
 };
 
+var multiply = function(number1, number2) {
+    return number1 * number2;
+};
+
 var number1 = parseInt(prompt("Input number:"));
 var number2 = parseInt(prompt("Input another number:"));
 
